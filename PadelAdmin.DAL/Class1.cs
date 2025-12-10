531454a2890521485a881a169ef0b8320f49d478
Jaime Pérez Roget Blanco <xaimeprb@gmail.com>
2025-12-10 22:04:23 +0100

    Agregar archivos de proyecto.

---
+++ PadelAdmin.DAL/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadelAdmin.DAL
+{
+    public class Database
+    {
+
+        private static Database instance;
+        private readonly string connectionString = "Server=localhost;Database=PadelAdmin;Trusted_Connection=True;";
+
+        private Database() { }
+
+        public static Database Instance
+        {
+
+            get
+            {
+
+                if(instance == null)
+                {
+
+                    instance = new Database();
+
+                }
+
+                return instance;
+
+            }
+
+        }
+
+        public SqlConnection GetConnection()
+        {
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+            return conn;
+
+        }
+
+    }
+}
---
+++ PadelAdmin.UI/Form1.cs
@@ -0,0 +1,10 @@
+namespace PadelAdmin.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
